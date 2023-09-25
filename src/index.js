5905cd2c977f492481c2e4b293c939e2e602160c
wss <wangss@anchnet.com>
2023-09-25 18:06:11 +0800

    fix: :bug: 测试emoji

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import App from './App';
 import store from './store';
 import './assets/css/index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(1);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
